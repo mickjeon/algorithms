aa88406cbc35555cecd1992a77cff935143d4a5d
mickjeon <mickjeon@gmail.com>
2023-10-07 17:04:14 -0700

    740 done

--- DP/740.py
+++ DP/740.py
@@ -1,24 +1,33 @@
-def deleteAndEarn(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        Original idea:
-            3 4 2
-            if i delete 3, [] ==> 3
-            if i delete 4, [2] ==> 4
+def deleteAndEarn(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    - build from 0 to max(nums)
+    - dp[0] = 0
+    - dp[1] = 1 * #1's occurence
+    - dp[i] = max(dp[i - 1], dp[i - 2] + currGain)
+                don't take curr vs. take curr
+    
+    Main takeaways:
+        - take vs. don't take
+        - incontinuous data can work if used
+        - counter returns 0 if key is not found
+    """
+    from collections import Counter
 
-            2 3 4 (sorted)
-            if i delete 2, [4] f([2,3,4]) = 2 + f(4)
-            delete 3, f(2,3,4) = 3 + f([])
-            delete 4, f(2,3,4) = 4 + f([2])
-            max of these and use memoization.
-            BUT, cannot hash lists
-        New approach:
-        - the frequency of each num is the score contributor
-        """
-        pass
+    info = Counter(nums)
+    for key in info.keys():
+        info[key] = info[key] * key
+    max_num = max(nums)
+    dp = [0] * (max_num + 1)
+    dp[0] = 0
+    dp[1] = info[1]
+    
+    for i in range(2, max_num + 1):
+        dp[i] = max(dp[i - 1], info[i] + dp[i - 2])
+    return dp[max_num]
 
 if __name__ == '__main__':
-    arr = [3,4,2]
+    # arr = [3,4,2]
     arr = [2,2,3,3,3,4]
     print(deleteAndEarn(arr))
